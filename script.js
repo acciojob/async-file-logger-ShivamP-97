f101f89cab29db82dc39158978ccdd3b944c8275
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-27 14:29:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ const fileNames = [
 
 // do not change the code above
 // add your code here
+fileNames.forEach(file => {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch(err => console.error("Error reading:", file, err));
+});
